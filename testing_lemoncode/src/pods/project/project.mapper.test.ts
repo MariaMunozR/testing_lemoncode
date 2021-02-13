5e26c72d9e868c0d3e5391fbe391455a21eb4dd9
MariaMunozR <maria.munoz.rubio@gmail.com>
2021-02-13 10:35:42 +0100

    add 1st test file

---
+++ testing_lemoncode/src/pods/project/project.mapper.test.ts
@@ -0,0 +1,19 @@
+import * as apiModel from './api/project.api-model';
+import * as viewModel from './project.vm';
+import { mapProjectFromApiToVm } from './project.mapper';
+
+describe('project mapper spec', () => {
+  it('should return a clone object whit "viewModel" interface', () => {
+    //Arrange
+    //Act
+    //Assert
+  });
+});
+
+/*
+//Arrange
+
+//Act
+
+//Assert
+*/
