81b684362b29bfcf1846cee69667dab3665261fb
MariaMunozR <maria.munoz.rubio@gmail.com>
2021-02-14 11:39:03 +0100

    add debuger

--- testing_lemoncode/src/pods/project/project.mapper.test.ts
+++ testing_lemoncode/src/pods/project/project.mapper.test.ts
@@ -2,7 +2,7 @@ import * as apiModel from './api/project.api-model';
 import * as viewModel from './project.vm';
 import { mapProjectFromApiToVm } from './project.mapper';
 
-describe('project mapper spec', () => {
+describe('project mapper test', () => {
   it('should return a clone object whit "viewModel" interface', () => {
     //Arrange
     //Act
