7d4a654e95a1e8b3e24eebd77ddade6d949f35e4
MariaMunozR <maria.munoz.rubio@gmail.com>
2021-02-19 20:16:24 +0100

    add tests to confirmation dialog

--- testing_lemoncode/src/common/components/confirmation-dialog/confirmation-dialog.hook.spec.ts
+++ testing_lemoncode/src/common/components/confirmation-dialog/confirmation-dialog.hook.spec.ts
@@ -3,19 +3,52 @@ import { useConfirmationDialog } from './confirmation-dialog.hook';
 import { Lookup } from 'common/models';
 
 describe('useConfirmationDialog specs', () => {
-  it('', () => {
+  it('should return properties when it is called', () => {
     //Arrange
+    const createEmptyLookup: Lookup = {
+      id: '',
+      name: '',
+    };
     //Act
+    const { result } = renderHook(() => useConfirmationDialog());
+
     //Assert
+    expect(result.current.isOpen).toEqual(false);
+    expect(result.current.itemToDelete).toEqual(createEmptyLookup);
+    expect(result.current.onAccept).toEqual(expect.any(Function));
+    expect(result.current.onClose).toEqual(expect.any(Function));
+    expect(result.current.onOpenDialog).toEqual(expect.any(Function));
   });
-  it('', () => {
+
+  it('should return isOpen = true & itemToDelete = item when onOpenDialog is called with an item', () => {
     //Arrange
+    const item: Lookup = {
+      id: '000',
+      name: 'Test name',
+    };
     //Act
+    const { result } = renderHook(() => useConfirmationDialog());
+
+    act(() => {
+      result.current.onOpenDialog(item);
+    });
     //Assert
+    expect(result.current.isOpen).toEqual(true);
+    expect(result.current.itemToDelete).toEqual({
+      id: '000',
+      name: 'Test name',
+    });
   });
-  it('', () => {
+
+  it('should return isOpen = false when it is called onClose', () => {
     //Arrange
+
     //Act
+    const { result } = renderHook(() => useConfirmationDialog());
+    act(() => {
+      result.current.onClose();
+    });
     //Assert
+    expect(result.current.isOpen).toEqual(false);
   });
 });
